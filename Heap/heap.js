7fa3a85d4dbd9fec1ad8b30c1241a7ca3c37af43
yiyang.sy <yiyang.sy@antfin.com>
2019-09-20 17:16:36 +0800

    修正取最后一个非叶子节点的逻辑

--- Heap/heap.js
+++ Heap/heap.js
@@ -6,11 +6,11 @@
  * 堆可以用数组表示
  * 特性：第n个节点的左子节点为2n+1  右子节点为2n+2
  * 如果想找到最后一个非叶子节点
- * 1）最后一个非叶子节点只有左孩子
- * 2）最后一个非叶子节点有左右孩子
+ * 1）最后一个非叶子节点只有左孩子  节点总数为偶数
+ * 2）最后一个非叶子节点有左右孩子  节点总数为奇数
  * 
- * 1) 左孩子为 length - 1; length - 1 = 2n + 2   -->   n = (length - 3) / 2
- * 2) 左孩子为 length - 2; length - 2 = 2n + 1   -->   n = (length - 3) / 2
+ * 1) 左孩子为 length - 1; length - 1 = 2n + 1   -->   n = (length - 2) / 2    
+ * 2) 左孩子为 length - 2; length - 2 = 2n + 1   -->   n = (length - 3) / 2  length为奇数  等价于 Math.floor((length - 3 + 1) / 2)
  * 
  * 所以最后一个非叶子节点n 为 n = (length - 3) / 2;
  */
@@ -19,7 +19,7 @@ const testArr = [7,6,8,3,9,5,10,2,4,1,11,2,6,8,6];
 
 const creatMaxHeap = (arr) => {
   let len = arr.length;
-  let lastRootNodeIndex = (len - 3) / 2;
+  let lastRootNodeIndex = Math.floor((len - 3 + 1) / 2) / 2;
   for (let i = lastRootNodeIndex; i >= 0; i--) { 
     // 从最后一个非叶子节点往前遍历知道根节点0，判断父节点和最大且大于自己的子节点交换位置
     let leftChildIndex = 2 * i + 1;
