cece45514209d4d4d7cb9d4f9c99373d2c53f928
yysy-fe <468068459@qq.com>
2019-09-24 11:21:20 +0800

    优化二叉树dfs & 增加N叉树dfs

--- Tree/dfs.js
+++ Tree/dfs.js
@@ -62,23 +62,74 @@ console.log(res);
 
 const dfs = tree => {
   let result = [];
-  let stack = [];
   let current = tree;
-  while (current || stack.length > 0) {
-
-    // 先把left遍历完
-    while (current) {
-      stack.push(current);
-      result.push(current.value);
-      current = current.left;
-    }
-    
-    // 栈后入先出，所以left全部遍历完后，取父节点的右孩子
-    let parent = stack.pop();
-    current = parent.right;
+  let stack = [current];
+  while (stack.length > 0) {
+    current = stack.pop();
+    result.push(current.value);
+    current.right && stack.push(current.right);
+    current.left && stack.push(current.left);
   }
   return result;
 }
 
 const result = dfs(treeData);
-console.log(result)
+console.log('二叉树', result)
+
+const testTree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 5,
+        },
+        {
+          value: 6
+        }
+      ]
+    },
+    {
+      value: 3,
+      children: [
+        {
+          value: 7
+        }
+      ]
+    },
+    {
+      value: 4,
+      children: [
+        {
+          value: 8
+        }
+      ]
+    }
+  ]
+};
+
+/**
+ *             1
+ *          2  3  4
+ *         5 6 7  8
+ */
+
+const dfs1 = tree => {
+  let current = tree, stack = [current], result = [];
+  while (stack.length > 0) {
+
+    // 推出栈尾，为父元素的正序孩子
+    current = stack.pop();
+    result.push(current.value);
+    let children = current.children || [];
+    for (let i = children.length - 1; i >= 0; i--) {
+      // 栈是children倒序
+      stack.push(children[i]);
+    }
+  }
+  return result;
+}
+
+const result1 = dfs1(testTree);
+console.log('N叉树', result1);
